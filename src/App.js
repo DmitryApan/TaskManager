b9b652253b8d690d9ca1f158e827592a24a5f442
Dzmirty Apanasevich <dmitry.apan@yandex.by>
2020-06-14 19:30:18 +0300

    added few line code

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ class App extends React.Component {
 
         let response = await login(body);
 
-        if (response.ok) {
+        if (response.email) {
             this.setState({
                 isLogin: true
             });
