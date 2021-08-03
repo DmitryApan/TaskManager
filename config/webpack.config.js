a8f15ae28bf20549afcf523e829eeb18cdb6473d
Dzmirty Apanasevich <dmitry.apan@yandex.by>
2021-08-03 09:53:02 +0300

    fixed bugs

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -568,7 +568,8 @@ module.exports = function(webpackEnv) {
           exclude: [/\.map$/, /asset-manifest\.json$/],
           importWorkboxFrom: 'cdn',
           navigateFallback: publicUrl + '/index.html',
-          navigateFallbackBlacklist: [
+          //navigateFallbackBlacklist: [
+            navigateFallbackDenylist: [
             // Exclude URLs starting with /_, as they're likely an API call
             new RegExp('^/_'),
             // Exclude URLs containing a dot, as they're likely a resource in
